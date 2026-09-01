fa4c43ebd7892e061d2812f45dfd78f7614f11f1
Jean Luc POIRIER <6810558f@commun.ad.sncf.fr>
2026-09-01 08:28:54 +0200

    version

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const APP_VERSION = "v100";
-const APP_VERSION_DATE = "01/09/2026 07:19";
+const APP_VERSION_DATE = "01/09/2026";
 const appVersionEl = document.getElementById("appVersion");
 
 if (appVersionEl) {
