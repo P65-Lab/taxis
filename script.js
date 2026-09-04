2fb1cc4f9544377cba32f7c44c60c30f8c55fd57
Jean Luc POIRIER <6810558f@commun.ad.sncf.fr>
2026-09-04 22:17:16 +0200

    DESCRIPTIF DES MISES A JOUR

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
 const APP_VERSION = "v2";
+
 const APP_VERSION_DATE = "04/09/2026";
+
+const APP_UPDATE_NOTES = [
+  "Nouveau système de mise à jour",
+  "Ajout du point rouge sur Paramètres",
+  "Ajout du bouton Mise à jour disponible dans Paramètres",
+  "Possibilité de reporter une mise à jour"
+];
 const appVersionEl = document.getElementById("appVersion");
 
 if (appVersionEl) {
@@ -4005,8 +4013,24 @@ let miseAJourDemandee = false;
 function afficherMiseAJour(worker) {
 
   if (!worker) return;
+/* DETAILS DE LA MISE A JOUR */
 
-  /* POINT ROUGE */
+const updateText =
+  document.getElementById("updateText");
+
+if (updateText) {
+
+  updateText.innerHTML =
+    `<strong>Version ${APP_VERSION}</strong><br>` +
+    `<small>Du ${APP_VERSION_DATE}</small><br><br>` +
+    `<strong>Modifications :</strong><br>` +
+    APP_UPDATE_NOTES
+      .map(note => `• ${note}`)
+      .join("<br>");
+
+}
+ 
+/* POINT ROUGE */
   if (updateBadge) {
     updateBadge.hidden = false;
   }
