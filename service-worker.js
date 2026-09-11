a57d76e15e01b4c2513e9f551c7221e238f037c3
Jean Luc POIRIER <6810558f@commun.ad.sncf.fr>
2026-09-11 20:22:18 +0200

    Essai MàJ

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "taxi-pwa-cache-v1";
+const CACHE_NAME = "taxi-pwa-cache-v2";
 
 
 /* ==========================================================
@@ -6,7 +6,7 @@ const CACHE_NAME = "taxi-pwa-cache-v1";
    ========================================================== */
 
 const UPDATE_INFO = {
-  version: "v1",
+  version: "v2",
   date: "011/09/2026",
 
   notes: [
