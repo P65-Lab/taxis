92c074687ef6aa4a9628652bb783054f14779f91
Jean Luc POIRIER <6810558f@commun.ad.sncf.fr>
2026-09-15 13:13:00 +0200

    V

--- version.js
+++ version.js
@@ -2,9 +2,9 @@
    VERSION DE L'APPLICATION TAXI
    ========================================================== */
 
-globalThis.TAXI_VERSION = "v1.8";
+globalThis.TAXI_VERSION = "v1.9";
 
-globalThis.TAXI_VERSION_DATE = "12/09/2026";
+globalThis.TAXI_VERSION_DATE = "15/09/2026";
 
 globalThis.TAXI_UPDATE_NOTES = [
   "Test de l'affichage du détail avant installation",
